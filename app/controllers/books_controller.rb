8f9991839bbd604b8fbb665f3f283b9200443cff
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-03-17 12:36:16 +0000

    [start]応用課題２

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+  
+ before_action :correct_user, only: [:edit,:update]
+
+  def show
+    @book = Book.find(params[:id])
+    @user = User.find_by(id: @book.user_id)
+    
+  end
+
+  def index
+    @books = Book.all
+    @book =  Book.new
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), success: "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+  
+  def correct_user
+   @book=Book.find(params[:id])
+   @user=@book.user
+   redirect_to books_path unless @user == current_user
+  end
+ 
+end
